de00f93d3f81eca21dd51444045f4deccf10763d
Fimat08 <ahmed.saddi@gmail.com>
2022-05-28 17:46:42 +0200

    Update Ultrasonic.py

--- Code_Jason/Ultrasonic.py
+++ Code_Jason/Ultrasonic.py
@@ -8,9 +8,9 @@ ECHO = 24
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 GPIO.output(TRIG, False)
-print ('Waiting a few seconds for the sensor to settle')
 while(1):
     time.sleep(2)
+    print ('Waiting a few seconds for the sensor to settle')
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
