4b629ef95ab224e61bc1539aae3537ae67b660b9
Fimat08 <ahmed.saddi@gmail.com>
2022-05-29 16:36:44 +0200

    Update test_motor.py

--- Code_Jason/test_motor.py
+++ Code_Jason/test_motor.py
@@ -6,6 +6,7 @@ import time    # Import necessary modules
 
 if __name__ == '__main__':
 	m=Motor()
+	m.setSpeed(50)
 	time.sleep(2)
 	m.forward()
 	time.sleep(2)
@@ -17,6 +18,7 @@ if __name__ == '__main__':
 	time.sleep(2)
 	m.turn_right()
 	time.sleep(2)
+	m.turn_center()
 	m.stop()
 
-	
+
