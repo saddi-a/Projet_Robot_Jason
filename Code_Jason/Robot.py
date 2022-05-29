6762f9b613283bc6a4ff06bb63b21fd41b6dcf84
Fimat08 <ahmed.saddi@gmail.com>
2022-05-29 14:40:17 +0200

    Create Robot.py

---
+++ Code_Jason/Robot.py
@@ -0,0 +1,44 @@
+import time
+import RPi.GPIO as GPIO
+from Camera import Camera
+from Motor import Motor
+from Ultrasonic import Ultrasonic
+
+class Robot:
+
+    def __init__(self):
+
+        self.motor=Motor()
+        self.camera=Camera()
+        self.ultrasonic=Ultrasonic()
+
+    
+    def readSensors(self):
+        self.camera.readLight()
+        print("The traficlight color is :",self.camera.trafficlightColor)
+        self.ultrasonic.measure()
+        print("The distance is:", self.ultrasonic.distance)
+
+    def move(self):
+
+        for i in range(200) :
+
+            if (self.camera.trafficlightColor=='red' or  self.ultrasonic.distance<10):
+                self.motor.stop()
+            elif(self.camera.trafficlightColor=='orange' or  self.ultrasonic.distance<20):
+                self.motor.setSpeed(20)
+                self.motor.forward()
+            elif(self.camera.trafficlightColor=='green'):
+                self.motor.setSpeed(50)
+                self.motor.forward()
+            else:
+                self.motor.setSpeed(30)
+                self.motor.forward()
+
+
+            time.sleep(0.1)
+            print(i)
+
+        GPIO.cleanup()
+        GPIO.setwarnings(True)
+
